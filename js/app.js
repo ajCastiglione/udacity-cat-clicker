46e050021dd5f6cdf46c07ab58d2520dacf2ba16
Aj Castiglione <antoniocastiglione10@gmail.com>
2018-03-14 21:35:22 -0400

    Modified names of cats object to be appropriate

--- js/app.js
+++ js/app.js
@@ -4,30 +4,29 @@ $(function() {
   const catSelect = $('#catSelection');
   const chosenCat = $('#selectedCat');
   const menu = $('.menu-title span');
-  catSelect.hide();
 
   menu.on('click', () => {
     catSelect.slideToggle(500);
   });
 
   let cats = [{
-    name: 'Stinky',
+    name: 'Ashes',
     url: 'https://lh3.ggpht.com/nlI91wYNCrjjNy5f-S3CmVehIBM4cprx-JFWOztLk7vFlhYuFR6YnxcT446AvxYg4Ab7M1Fy0twaOCWYcUk=s0#w=640&h=426',
     clicks: 0
   }, {
-    name: 'Smelly',
+    name: 'Tiger',
     url: 'https://lh3.ggpht.com/kixazxoJ2ufl3ACj2I85Xsy-Rfog97BM75ZiLaX02KgeYramAEqlEHqPC3rKqdQj4C1VFnXXryadFs1J9A=s0#w=640&h=496',
     clicks: 0
   }, {
-    name: 'Dildo',
+    name: 'Puss',
     url: 'https://lh5.ggpht.com/LfjkdmOKkGLvCt-VuRlWGjAjXqTBrPjRsokTNKBtCh8IFPRetGaXIpTQGE2e7ZCUaG2azKNkz38KkbM_emA=s0#w=640&h=454',
     clicks: 0
   }, {
-    name: 'Fucker',
+    name: 'Smokey',
     url: 'https://static.pexels.com/photos/20787/pexels-photo.jpg',
     clicks: 0
   }, {
-    name: 'Cunt Nugget',
+    name: 'Misty',
     url: 'https://static.pexels.com/photos/177809/pexels-photo-177809.jpeg',
     clicks: 0
   }];
@@ -37,6 +36,7 @@ $(function() {
     catSelect.append("<div class='catChoice'><p id='" + i + "'>" + cats[i].name + "</p><br>" + "<img src='" + cats[i].url + "'></div>");
   }
 
+  // Once a cat is clicked: get the index, populate the chosenCat section with the selected cat, if mobile - slide menu up once a cat is selected.
   catSelect.on('click', 'div', function() {
     let catIndex = $(this).children('p').attr('id');
     chosenCat.html("<div class='selectedKitty'><h3 class='cat-title' id='" + catIndex + "'>" + cats[catIndex].name + "</h3><img src='" + cats[catIndex].url + "'><p class='clickCounter'>" + cats[catIndex].clicks + "</div>");
@@ -45,6 +45,7 @@ $(function() {
     }
   });
 
+  // Once the chosenCat is clicked: get the index, add 1 to the click counter, update the clicks on the screen.
   chosenCat.on('click', 'div', function() {
     let clickIndex = $(this).children('h3').attr('id');
     cats[clickIndex].clicks += 1;
